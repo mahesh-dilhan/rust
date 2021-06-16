2e91f780559d99de22581bfe13131dfdb46b5dbd
mahesh <mail.mahesh.lk@gmail.com>
2021-06-16 09:15:08 +0800

    rust-basics

---
+++ rust-std/target/debug/deps/rust_std.d
@@ -0,0 +1,5 @@
+/Users/mahesh/play/rust/rust-std/target/debug/deps/rust_std: src/main.rs
+
+/Users/mahesh/play/rust/rust-std/target/debug/deps/rust_std.d: src/main.rs
+
+src/main.rs:
---
+++ rust-std/target/debug/rust-std.d
@@ -0,0 +1,1 @@
+/Users/mahesh/play/rust/rust-std/target/debug/rust-std: /Users/mahesh/play/rust/rust-std/src/main.rs
