4e45df64ad7395a65e1cdfb3154ebbc6828e995a
mahesh <mail.mahesh.lk@gmail.com>
2021-07-06 19:28:25 +0800

    rust fn

--- rust-std/src/fn/main.rs
+++ rust-std/src/fn/main.rs
@@ -2,7 +2,13 @@ fn main() {
 
     println!("this is main");
     another_function();
+    println!("{}",retfive());
 }
 fn another_function(){
     println!("this is in another_function");
+}
+
+fn retfive() -> i32 {
+    let x=2;
+    return x;
 }
