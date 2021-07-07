c89f9285a23498ae64ffe4be92461909c2910086
mahesh <mail.mahesh.lk@gmail.com>
2021-07-07 17:15:49 +0800

    rust-controlflow

---
+++ rust-std/src/controlflow/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let x =10;
+    if x < 5 {
+        println!("below 5")
+    } else if x >7 && x< 9    {
+        println!("between 7 and 9")
+    }
+    else {
+        println!("above 9")
+    }
+}
