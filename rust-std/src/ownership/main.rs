758524fa024ec8a3451d3d0419db7728b0634c6c
mahesh <mail.mahesh.lk@gmail.com>
2021-07-07 19:45:26 +0800

    rust-ownership

---
+++ rust-std/src/ownership/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let s1 = String::from("hello");
+    let s2 = s1;
+    let s1 = s2;
+    println!("{}, world!", s1);
+    println!("{}, world!", s2);// will not work as this s2 already moved //
+}
