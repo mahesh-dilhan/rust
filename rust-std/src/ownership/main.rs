dc588139a83770dc579597a195994da0585e43c7
mahesh <mail.mahesh.lk@gmail.com>
2021-07-07 19:47:28 +0800

    rust-ownership

--- rust-std/src/ownership/main.rs
+++ rust-std/src/ownership/main.rs
@@ -3,5 +3,12 @@ fn main() {
     let s2 = s1;
     let s1 = s2;
     println!("{}, world!", s1);
-    println!("{}, world!", s2);// will not work as this s2 already moved //
+   // println!("{}, world!", s2);// will not work as this s2 already moved //
+
+
+    let y1 = String::from("hello");
+    let y2 = y1;
+
+    //println!("{}, world!", y1);
+    println!("{}, world!", y2)
 }
